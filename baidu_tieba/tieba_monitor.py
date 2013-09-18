a8bcdfcb13addefc4075f6609e3d5bd69edd56cf
auchan <506747762@qq.com>
2013-09-18 20:45:11 +0800

    alpha-1.1

--- baidu_tieba/tieba_monitor.py
+++ baidu_tieba/tieba_monitor.py
@@ -20,9 +20,10 @@ command bar_name display_amount
         amount = int(argv[1])
 
     abs_url = 'http://tieba.baidu.com/' + str(gbk_code.getGBKCode(s=rel_url))
+
+    print('开始解析网页\n')
     print('Site:', abs_url)
 
-    count, skip = 0, 0
     find = False
     for line in urlopen(abs_url):
         line = line.decode('gbk')
@@ -33,23 +34,46 @@ command bar_name display_amount
                 print('Title:', titles[0])
                 print()
         # subject symbol
-        if 'j_thread_list clearfix' in line or find:
-            find = True
-            # article title
-            if u'class="j_th_tit"' in line:
+        if 'j_thread_list thread_top clearfix' in line:
+            not_top = re.findall('j_thread_list thread_top clearfix ".*?</li>(.*)', line)
+            if len(not_top) != 1:
+                raise ValueError('内容可能出错')
+
+            contents = re.findall(r'class="threadlist_text.*?">(.*?)<s', not_top[0])
+            authors = re.findall(r'class="tb_icon_author" title="主题(.*?)"', not_top[0])
+            last_replyers = re.findall(r'tb_icon_author_rely j_replyer" title="(.*?)"', not_top[0])
+            last_rep_dates = re.findall(r'threadlist_reply_date j_reply_data" title="最后回复时间">(.*?)</', not_top[0])
+
+            count = 0
+            for l in contents:
+                if count % 2 == 0:
+                    title = re.findall(r'title="(.*?)"', l)[0]
+                    # 一个汉字的宽度等于两个ascii的宽度
+                    # 百度贴吧标题最长为30个汉字
+                    ascii_num = 0
+                    for index in title:
+                        if ord(index) < 128:
+                            ascii_num += 1
+                    sup = 62 - (2*len(title) - ascii_num)
+                    print('[{}] {}'.format(count//2 + 1, title) + '.'*sup + '{}'.format( 
+                        authors[count//2])) 
+                else:
+                    summary = re.findall(r'.*threadlist_abs_onlyline"\>(.*?)\<!--', l)
+                    if summary:
+                        summary = summary[0]
+                    else:
+                        summary = ''
+                    print(' '*4 + '({})'.format(summary))
+                    print('{}  {}\n'.format(last_replyers[(count-1)//2],
+                        last_rep_dates[(count-1)//2]))
                 count += 1
-                th_title = re.findall(r'href.*?title="(.*?)"', line)
-                print('[{}]'.format(count), th_title[0])
-            # article summary
-            if u'threadlist_abs threadlist_abs_onlyline' in line:
-                skip += 1
-                if skip == 2:
-                    summarys = re.findall(r'\<div.*?\>(.*)(\<!--+>)+\</div\>', line)
-                    print('summary:', summarys[0][0])
-                    print()
-                    find = False
-                    skip = 0
-        # list first N number arcticls, where N is specifiled by amount
-        # N must have a uppe bound for I'm not consider about.
-        if count == amount and find == False:
+                # list first N number arcticls, where N is specifiled by amount
+                # N must have a uppe bound for I'm not consider about.
+                if count >= 2*amount:
+                    break
+            print('解析结束.')
             break
+
+    print('  '*10 + '#')
+    print('.'*10 + '#')
+    print('人'*10 + '#')
